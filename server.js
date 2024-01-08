43ee2f7dcd8d13b83ecb327ca8964b24c0b4368d
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-01-08 18:27:14 -0500

    Moved files around for Heroku deployment

--- server.js
+++ server.js
@@ -5,29 +5,32 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const { v4: uuid } = require("uuid");
 
-app.use(express.static('./Develop/public'));
+app.use(express.static('./public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, './Develop/db/db.json'), "utf8", (err, data)=>{
+    fs.readFile(path.join(__dirname, '/db/db.json'), "utf8", (err, data)=>{
+        if (err) {
+            console.info(err)
+            res.status(500).send("Error in reading notes.")
+        } else {
+            res.send(data)
+        }
 
-        console.log(data)
-        res.send(data)
     })
 })
 
 app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-
+  
   const title = req.body.title;
   const text = req.body.text;
 
@@ -40,20 +43,18 @@ app.post('/api/notes', (req, res) => {
       id: uuid(), //assigns a unique id to each note
     };
 
-    //const noteString = JSON.stringify(newNote); // converts array/objects into a string in json convention {"a":"1"}
-
-    let prevJSON = fs.readFileSync(path.join(__dirname, './Develop/db/db.json'), "utf8")
+    let prevJSON = fs.readFileSync(path.join(__dirname, '/db/db.json'), "utf8")
     let allNotes = JSON.parse(prevJSON) // array
     allNotes.push(newNote)
 
-    let newJSON = JSON.stringify(allNotes)
+    let newJSON = JSON.stringify(allNotes) // converts array/objects into a string in json convention
     
-    fs.writeFile('./Develop/db/db.json', newJSON, (err) => {
+    fs.writeFile('/db/db.json', newJSON, (err) => {
         if (err) {
             console.info(err)
             res.status(500).send("Error in saving note.")
         } else {
-            console.info("Successfully added a new note.");
+            console.info(`${req.method} request received; Successfully added a new note.`);
             res.status(200).send(newNote)
         }
 
@@ -62,9 +63,18 @@ app.post('/api/notes', (req, res) => {
 });
 
 // Delete notes
-app.delete('/notes/:id', (req, res) => {
+// app.delete('/notes/:id', (req, res) => {
+//     fs.readFile(path.join(__dirname, './Develop/db/db.json'), "utf8", (err, data)=>{
+//         if (err) {
+//             console.info(err)
+//             res.status(500).send("Error in reading notes.")
+//         } else {
+//             res.send(data)
+//         }
+//     let notes = notes.JSON.parse(data);
 
-});
+//     })
+// })
 
 
 app.listen(PORT, () => {
